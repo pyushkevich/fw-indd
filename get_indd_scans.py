7e1cd4156e3f7d6a29befad7ca158886d68d6f9d
Paul Yushkevich <pyushkevich@gmail.com>
2020-01-23 10:28:47 -0500

    added caching functionality, new MRI and PET keys

--- get_indd_scans.py
+++ get_indd_scans.py
@@ -5,6 +5,16 @@ import flywheel
 import pydicom
 import sys
 import csv
+import re
+import logging
+import os
+import json
+import datetime
+
+# To support JSON dump
+def json_default_conv(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
 
 # A list of search paths in FlyWheel used to search for INDD subjects.
 # For now this is hard-coded
@@ -12,27 +22,65 @@ search_paths = ["cfn/PMC-CLINICAL", "dwolklab/NACC-SC"]
 
 # This function reads the first DICOM file found in an acquisition
 # It also returns the FW file reference
-def fw_parse_acq_dicom(acq):
-  
-  # Search for the first DICOM file
-  try:
-    f=next(x for x in acq.files if (lambda f: f.type=='dicom'))
-    zi=acq.get_file_zip_info(f.name)
-    fp=io.BytesIO(acq.read_file_zip_member(f.name, zi.members[0].path))
-    return pydicom.dcmread(fp, stop_before_pixels=True),f
-  except:
-    return None,None
+def fw_parse_acq_dicom(acq, dicom_cache=None):
+
+    # Check if the DCM exists in the cache
+    if dicom_cache:
+        fn_cache = os.path.join(dicom_cache, '%s.json' % acq.id)
+        if os.path.exists(fn_cache):
+            with open(fn_cache) as f_cache:
+                try:
+                    cached = json.load(f_cache)
+                    if cached.get('modtime', 0) == acq.modified.toordinal():
+                        # Load cached dicom and file
+                        dcm = pydicom.dataset.Dataset.from_json(cached.get('dicom', None))
+                        f = cached.get('file', None)
+                        return dcm,f
+                except (ValueError):
+                    pass
+
+    # Otherwise, read the DICOM from zip file
+    try:
+        # Get the first DICOM file
+        f=next(x for x in acq.files if (lambda f: f.type=='dicom'))
+        zi=acq.get_file_zip_info(f.name)
+        fp=io.BytesIO(acq.read_file_zip_member(f.name, zi.members[0].path))
+
+        # Read the DICOM
+        dcm = pydicom.dcmread(fp, stop_before_pixels=True)
+
+        # Place into cache if defined
+        if dicom_cache:
+            fn_cache = os.path.join(dicom_cache, '%s.json' % acq.id)
+            with open(fn_cache, 'wt') as f_cache:
+                cached = {
+                    'dicom': dcm.to_json(),
+                    'file': f.to_dict(),
+                    'modtime': acq.modified.toordinal()
+                };
+                json.dump(cached, f_cache, default=json_default_conv)
+
+        # Return result
+        return dcm, f
+
+    except (StopIteration):
+        logging.warning('No DICOM files in acquisition %s' % acq.id)
+        return None,None
+    except (flywheel.rest.ApiException):
+        logging.warning('FlyWheel API Exception in acquisition %s' % acq.id)
+        return None,None
+
 
 
 # This function reads dicom tags from the first acquisition
 # in a FlyWheel session. Tags can then be accessed as simple
 # attributes of the return object
 def fw_parse_session_dicom(sess):
-  
+
   # Get the first acquisition
   acq=sess.acquisitions.find_first()
   if acq is None:
-    return None,None
+      return None,None
 
   # Search for the first DICOM file
   return fw_parse_acq_dicom(acq)
@@ -49,6 +97,28 @@ def fw_get_session_modality(sess):
         return acq.files[0].modality if len(acq.files) > 0 else None
 
     return None
+
+
+# Function to filter INDD ids
+def fn_filter_inddid(subject_name):
+
+  # Strip INDD and IND from name
+  z = re.search('^IND{1,2}(.*)$',subject_name)
+  if z is not None:
+    subject_name = z.group(1)
+
+  # Try basic 6-digit pattern
+  z = re.search('^[0-9]{6}$',subject_name)
+  if z is not None:
+    return z.group()
+
+  # Try 8-digit pattern
+  z = re.search('^([0-9]{6})[\._\-]([0-9]{2})$',subject_name)
+  if z is not None:
+    return '%s.%s' % (z.group(1), z.group(2))
+
+  # Failed to match anything
+  return None
 
 
 # Get an iterator over all acquisitions of given type, optionally in a 
@@ -74,9 +144,11 @@ def fw_make_acq_modality_filter(client, modality, project_path, subject=None):
 
 # List of columns returned for each modality
 modality_cols = {
+    # Referring Physician, etc
     'COMMON': [
+        'INDDID',
         'FlywheelSubjectID',
-        'FlywheelSessionTimestamp',
+        'FlywheelSessionTimestampUTC',
         'FlywheelSessionURL',
         'FlywheelSessionInternalID',
         'FlywheelProjectInternalID',
@@ -89,21 +161,45 @@ modality_cols = {
         'DicomStationName',
         'DicomBodyPartExamined',
         'DicomStudyInstanceUID',
-        'DicomSeriesInstanceUID' ],
+        'DicomSeriesInstanceUID',
+        'DicomSliceThickness',
+        'DicomPixelSpacingX',
+        'DicomPixelSpacingY'
+
+    ],
+
+    # Add:
+    #   number of TRs
+    #   duration of scan (how?) 
+    #   make sure time is in correct time zone
+    #   Echo Number
+    #   Phase Encode Direction
 
     'MR' : [
         'DicomMagneticFieldStrength',
         'DicomSequenceName',
-        'DicomSliceThickness',
         'DicomRepetitionTime',
         'DicomEchoTime',
+        'DicomEchoNumbers',
         'DicomFlipAngle',
         'DicomNumberOfAverages',
-        'DicomSpacingBetweenSlices',
-        'DicomPixelSpacing'
+        'DicomAcquisitionNumber',
+        'DicomSpacingBetweenSlices'
         ],
 
-    'PT' : [ ]
+    # Tracer used
+    # ReconstructionMethod
+    # ScatterCorrectionMethod
+    # Attenuation Correction Method
+    # Isotope?
+    'PT' : [
+        'DicomReconstructionMethod',
+        'DicomScatterCorrectionMethod',
+        'DicomAttenuationCorrectionMethod',
+        'DicomRadiopharmaceutical',
+        'DicomRadionuclide'
+    ]
+
 }
 
 
@@ -112,8 +208,9 @@ def make_output_text(sess, fw_acq, fw_file, dcm, column):
     
     # Use dictionary for simple outputs
     action_dict = {
+        'INDDID' : sess['indd_id'],
         'FlywheelSubjectID' : sess['subject_id'],
-        'FlywheelSessionTimestamp' : sess['session_ts'],
+        'FlywheelSessionTimestampUTC' : sess['session_ts'],
         'FlywheelSessionInternalID' : fw_acq.parents.session,
         'FlywheelProjectInternalID' : fw_acq.parents.project,
         'FlywheelAcquisitionLabel' : fw_acq.label,
@@ -121,12 +218,35 @@ def make_output_text(sess, fw_acq, fw_file, dcm, column):
             (fw_acq.parents.project,fw_acq.parents.session)
     } 
 
+    val=None
     if column in action_dict:
         val=action_dict[column]
+
     elif column.startswith('FlywheelAcquisition'):
         key=column[len('FlywheelAcquisition'):]
-        fcl = fw_file.classification;
+        fcl = fw_file.get('classification', {});
         val = ';'.join(fcl[key]) if key in fcl else None
+
+    elif column.startswith('DicomPixelSpacing'):
+        spc= dcm.get('PixelSpacing');
+        # spc_arr = map(lambda x:float(x.strip("[]' ")), spc_str.split(','))
+        val = {
+            'DicomPixelSpacingX' : spc[0],
+            'DicomPixelSpacingY' : spc[1] 
+        } [column] if spc is not None else None
+
+    elif column == 'DicomRadiopharmaceutical':
+        rpis=dcm.get('RadiopharmaceuticalInformationSequence',None)
+        if rpis and len(rpis) > 0:
+            val = rpis[0].get('Radiopharmaceutical',None)
+
+    elif column == 'DicomRadionuclide':
+        rpis=dcm.get('RadiopharmaceuticalInformationSequence',None)
+        if rpis and len(rpis) > 0:
+            rcs = rpis[0].get('RadionuclideCodeSequence', None)
+            if rcs and len(rcs) > 0:
+                val = rcs[0].get('CodeMeaning', None)
+        
     elif column.startswith('Dicom'):
         val=dcm.get(column[5:], None)
     else:
@@ -137,7 +257,7 @@ def make_output_text(sess, fw_acq, fw_file, dcm, column):
 
 
 # Get a listing of all the MRI scans across projects
-def fw_list_acq(csv_stream, client, modality, project_path, col_list, subject=None):
+def fw_list_acq(csv_stream, client, modality, project_path, col_list, subject=None, dicom_cache=None):
 
     # Get a filter to search for all acquisitions of this type
     fw_filter = fw_make_acq_modality_filter(client, modality, project_path, subject)
@@ -159,15 +279,33 @@ def fw_list_acq(csv_stream, client, modality, project_path, col_list, subject=No
             # Load the session
             sess = client.get_session(sess_id)
 
+            # Check the subject against a name filter
+            indd_id = fn_filter_inddid(sess.subject.label)
+
+            # Print an error message
+            if indd_id is None:
+                logging.warning('Subject ID failed to match INDD filter: %s' % sess.subject.label)
+
             # Store the common information about this session
+            # NOTE: the timestamp is in UTC 
             sess_cache[sess_id] = {
+                'indd_id':indd_id,
                 'subject_id':sess.subject.label,
-                'session_ts':sess.timestamp
+                'session_ts':sess.timestamp.strftime("%Y-%m-%d %H:%M:%S")
             }
 
+        # Get the cached session
+        S = sess_cache[sess_id]
+
+        # Must have valid INDDID
+        if S['indd_id'] is None:
+            continue
+
         # Load the dicom for this acquisition, or skip this acq if not found
-        dcm,f = fw_parse_acq_dicom(acq)
+        dcm,f = fw_parse_acq_dicom(acq, dicom_cache)
         if dcm is None:
+            logging.warning('Unable to extract DICOM for acquisition %s in subject %s session %s' %
+                          (acq.label, S['subject_id'], S['session_ts']))
             continue
 
         # Get the output corresponding to each column
@@ -187,7 +325,9 @@ def fw_list_acq(csv_stream, client, modality, project_path, col_list, subject=No
 @click.option('--header/--no-header', '-H', help='Include a header in the CSV file', default=False)
 @click.option('--modality', '-m', type=click.Choice(['MRI', 'PET'], case_sensitive=False),
               help='Which modality to list', default='MRI')
-def get_indd_scans(key, subject, header, modality):
+@click.option('--logfile', '-l', help='File for logging errors and warnings')
+@click.option('--cache', '-c', help='Use a DICOM cache in the specified directory')
+def get_indd_scans(key, subject, header, modality, logfile, cache):
     """Get a listing of INDD scans in FlyWheel in CSV format"""
 
     with open(key, 'r') as keyfile:
@@ -210,8 +350,17 @@ def get_indd_scans(key, subject, header, modality):
     if header:
         csv_stream.writerow(columns)
 
+    # Set up logging
+    if logging:
+        logging.basicConfig(filename=logfile, level=logging.DEBUG)
+
+    # Set up cache directory
+    if cache:
+        if not os.path.exists(cache):
+            os.makedirs(cache)
+
     for sp in search_paths:
-        fw_list_acq(csv_stream, fw, mod_fw, sp, columns, subject)
+        fw_list_acq(csv_stream, fw, mod_fw, sp, columns, subject, cache)
 
 
 
